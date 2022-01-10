4a9e1d5b8daad0b0286b87a5f7623e927518432f
Rijad <rijad.comor@fsk.unsa.ba>
2022-01-10 14:05:09 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,20 @@
+const students = [
+];
+
+function ubacistudenta(a,b,c,d,e,f){
+    var student = {
+        ime: a,
+        prezime: b,
+        index: c,
+        smjer: d,
+        ciklus: e,
+        semestar: f
+    }
+
+    students.push(student);
+}
+
+function prikazi(a){
+    let student = students.find(student => student.index === a);
+    document.getElementById("ispis").innerHTML = student.ime + ' ' +student.prezime;
+}
